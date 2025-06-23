1a3d126c0b77110d72005c5f1926d74565d69dd3
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-06-23 16:21:59 +0530

    Added Data types of JS

---
+++ Data.js
@@ -0,0 +1,11 @@
+let city = "Bokaro";
+let pincode = 827013;
+const country = "India";
+
+console.log(typeof city);
+console.log(typeof pincode);
+console.log(typeof country);
+
+console.log("City = " +city);
+console.log("Pincode = " +pincode);
+console.log("Country = " +country);
